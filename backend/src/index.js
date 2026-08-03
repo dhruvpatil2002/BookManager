0a0cb31fb9ec10626d4c0bbed360f889b5fcd250
dhruvpatil2002 <dhruvpatil713@gmail.com>
2026-08-03 13:06:07 +0530

    ENV key changes to stable version

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,38 +16,17 @@ const allowedOrigins = [
 ];
 
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
+app.use(cors({
+  origin: [
+process.env.CLIENT_URL,   
+    "http://localhost:3000"                       
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
 
-      // allow requests without origin (Postman, mobile apps)
-      if (!origin) {
-        return callback(null, true);
-      }
+    
 
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-
-      return callback(new Error("Not allowed by CORS"));
-    },
-
-    credentials: true,
-
-    methods: [
-      "GET",
-      "POST",
-      "PUT",
-      "DELETE",
-      "OPTIONS"
-    ],
-
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization"
-    ],
-  })
-);
 
 
 app.use(express.json());
